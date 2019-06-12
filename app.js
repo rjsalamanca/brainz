042ebcba7c2ed2a27c736a0875ad9b1854d4063f
RJ Salamanca <salamancaronito@gmail.com>
2019-06-12 10:36:32 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express'),
+    path = require('path'),
+    cookieParser = require('cookie-parser'),
+    logger = require('morgan'),
+    es6Renderer = require('express-es6-template-engine'),
+    indexRouter = require('./routes/index'),
+    usersRouter = require('./routes/users');
+
+const app = express();
+
+app.set('views','./views');
+app.set('view engine', 'html');
+app.engine('html', es6Renderer);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express'),
+  router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('template', { 
+    locals:{
+      title: 'Express'
+    },
+    partials: {
+      partial:'partial-index'
+    }
+  });
+});
+
+module.exports = router;
