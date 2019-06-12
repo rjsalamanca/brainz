b948bd7360b219c65b63f7aad18a50f1739bb05d
Pierson Scarborough <piersonscarborough@Piersons-MacBook-Pro.local>
2019-06-12 11:49:13 -0400

    initial commit

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
+const bodyParser = require('body-parser');
+
+function authenticate(req,res,next){
+    if(req.session){
+        if(req.session.name){
+            next()
+        }else{
+            res.redirect('/add-user');
+        }
+    }else{
+        res.redirect('/add-user');
+    }
+};
+
+router.use(bodyParser.urlencoded({extended: false}));
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/add-user', (req,res) => {
+  res.render('partial-add-user');
+
+});
+
+router.post('/add-user', (req,res) =>{
+  let name = req.body.name;
+  let pass = req.body.pass;
+
+  if(req.session){
+      req.session.name = name;
+      req.session.pass = pass;
+  };
+
+  console.log(name);
+  console.log(pass);
+
+  res.status(200).send();
+});
+
 
 module.exports = router;
