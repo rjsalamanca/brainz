f05723a9983ef3cb4a1384095c900edf06be0d5f
hdannyyi <hdannyyi@gmail.com>
2019-06-13 14:41:46 -0400

    fixed psql statement

--- routes/scores.js
+++ routes/scores.js
@@ -4,17 +4,24 @@ const express = require('express'),
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
-    const getScores = await scoresModel.getAllScores();
-    const getUsers = await scoresModel.getUserId();
+    const easyRegScores = await scoresModel.getScores(1);
+    const medRegScores = await scoresModel.getScores(2);
+    const hardRegScores = await scoresModel.getScores(3);
+    const easyFlickScores = await scoresModel.getScores(4);
+    const medFlickScores = await scoresModel.getScores(5);
+    const hardFlickScores = await scoresModel.getScores(6);
+    const apocRegScores = await scoresModel.getScores(7);
     
     res.render('template', { 
         locals: {
             title: 'Scores',
-            scores: getScores,
-            users: getUsers
+            easy: easyRegScores,
+            med: medRegScores,
+            hard: hardRegScores,
+            apoc: apocRegScores
         },
         partials: {
-            partial:'partial-scores'
+            partial:'partial-regular-scores'
         }
     });
 });
