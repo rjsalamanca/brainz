c7697a7e9b5e09ab0306cd147ff42b0a70411e14
RJ Salamanca <salamancaronito@gmail.com>
2019-06-18 12:42:10 -0400

    added errors

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const express = require('express'),
     session = require('express-session'),
     cookieParser = require('cookie-parser'),
     logger = require('morgan'),
+    createError = require('http-errors'),
     es6Renderer = require('express-es6-template-engine'),
     indexRouter = require('./routes/index'),
     usersRouter = require('./routes/users'),
     gamesRouter = require('./routes/games'),
     scoresRouter = require('./routes/scores');
-    
-const app = express();
 
+const app = express();
 
 app.set('views','./views');
 app.set('view engine', 'html');
@@ -32,5 +32,19 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/games',gamesRouter);
 app.use('/scores', scoresRouter);
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+  
 
 module.exports = app;
