b610b99324d9a9e0786f259c97e046fc7de79b79
RJ Salamanca <salamancaronito@gmail.com>
2019-06-18 13:26:15 -0400

    updated tests

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ const chai = require('chai'),
 chai.use(chaiAsPromised).should();
 
 const Games = require('../models/games');
+const Kills = require('../models/killCount');
 
 describe('Games model tests:', () =>{
     it('Should get an object of game mode and difficulty is valid', async () => {
@@ -13,8 +14,16 @@ describe('Games model tests:', () =>{
         expect(getsObjModeDiff).to.be.an('object');
     });
 
-    it('test', async () => {
+    it('Adding a score shouldnt be undefined', async () => {
         const checkAdd = await Games.addScore('80','1000','1','1');
         expect(checkAdd).to.not.be.an('undefined')
+    });
+});
+
+describe('Kills model tests:', () => {
+    it('Should be able to get kills', async () => {
+        const killsInstance = new Kills(null,null,1)
+        const getKills = await killsInstance.checkKillCount();
+        expect(getKills).to.be.an('object');
     });
 });
